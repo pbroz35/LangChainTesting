c11af7dc5a080b3c40043cb6ec973162c1ad9936
Piotr <1brozek.peter@gmail.com>
2024-10-15 12:59:37 -0500

    virtual environment

---
+++ venv/LCEL.py
@@ -0,0 +1,19 @@
+#LangChain Expression Language (LCEL)
+
+import os
+import openai
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+from langchain.prompts import ChatPromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.schema.output_parser import StrOutputParser
+
+
+#pip install pydantic=1.10.8
+
+
+
+
