86b32462fb3e37080c14b45b87b13bbe40f51572
Piotr <1brozek.peter@gmail.com>
2024-10-15 12:45:22 -0500

    function calling example

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import os
 import openai
 from openai import OpenAI
@@ -5,7 +6,7 @@ from openai import OpenAI
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv()) # read local .env file
 
-# define a function
+# functions
 functions = [
     {
         "name": "get_current_weather",
@@ -21,13 +22,30 @@ functions = [
             },
             "required": ["location"],
         },
-    }
+    },
+    
+    {
+        
+        "name": "get_current_temperature",
+        "description": "Get the current temperature in a given location",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "location": {
+                    "type": "string",
+                    "description": "The city and state, e.g. San Francisco, CA",
+                },
+                "unit": {"type": "string", "enum": ["celsius", "fahrenheit"]},
+            },
+            "required": ["location"],
+        },    
+    },
 ]
 
 messages = [
     {
         "role": "user",
-        "content": "What's the weather like in Boston?"
+        "content": "What's the weather like in Boston in fahrenheit?"
     }
 ]
 
@@ -42,7 +60,35 @@ client = OpenAI()
 response = client.chat.completions.create(
     model="gpt-4o-mini",
     messages=messages,
-    functions=functions
+    functions=functions,
+    function_call ="auto", # "auto" can choose to force the model to call a function "none" - forces model to NOT use any function calls 
 )
 
+print(response)
+
+
+function_call = response.choices[0].message.function_call
+args = json.loads(function_call.arguments)
+
+
+def get_current_weather(location): #assuming location is Boston Ma, function would need to get actual weather based off a location
+    return "The current weather is sunny and in the high 80's."
+
+
+#simulate calling the function based off the ai response (function would need to be defined)
+observation = get_current_weather(args.get("location"))
+
+messages.append(
+    {
+         "role": "function",
+         "name": "get_current_weather",
+         "content": observation,
+     }
+ )
+#
+response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=messages,
+ )
+   
 print(response)
